a02f2e2f51617af51e47fcfc17b1429746081c17
Eduardo Oliveira <eoliveira1986@gmail.com>
2023-06-18 11:50:53 -0300

    Add project files.

---
+++ test/Porter2023_test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Porter2023_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
