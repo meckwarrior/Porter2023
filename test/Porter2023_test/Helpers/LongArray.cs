3d95770b2bd84e48df9a4d51f2b0c09ab1e0397a
Eduardo Oliveira <eoliveira1986@gmail.com>
2023-06-20 16:13:04 -0300

    Teste unitário para Calculadora que soma os número do Array.

---
+++ test/Porter2023_test/Helpers/LongArray.cs
@@ -0,0 +1,14 @@
+﻿namespace Porter2023_test.Helpers
+{
+    public static class LongArray
+    {
+        public static int[] ArrayUmMilhao => RetornaArrayUmMilhao();
+        public static long ResultadoArrayUmMilhao => 500000500000;
+
+
+        private static int[] RetornaArrayUmMilhao()
+        {
+            return File.ReadAllLines("../../../Data/ArrayUmMilhao.txt").Select(int.Parse).ToArray();
+        }
+    }
+}
