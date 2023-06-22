042f3f81f5492c663284e2fea65965150dbb05cc
Eduardo Oliveira <eoliveira1986@gmail.com>
2023-06-22 01:50:58 -0300

    Criação da função para remover duplicados de uma lista de objetos.

---
+++ src/Porter2023/Libraries/ComparadorObjetos.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Text.Json;
+
+namespace Porter2023.Libraries
+{
+    public class ComparadorObjetos<T>
+    {
+        public static IList<T> RemoverDuplicados(IList<T> listaObjetos)
+        {
+            IList<T> result = new List<T>();
+
+            foreach(T objeto in listaObjetos) 
+            {
+                if(!EstaNaLista(result, objeto))
+                    result.Add(objeto);
+                
+            }
+
+            return result;
+        }
+
+        private static bool EstaNaLista(IList<T> listaObjetos, T objeto)
+        {
+            if(typeof(T).FullName.StartsWith("System"))
+                foreach (T _objeto in listaObjetos)
+                {
+                    if (_objeto.ToString() == objeto.ToString())
+                        return true;
+                }
+
+            else
+                foreach (T _objeto in listaObjetos)
+                {
+                    if (JsonSerializer.Serialize(_objeto)
+                        == JsonSerializer.Serialize(objeto))
+                    return true;
+                }
+
+            return false;
+        }
+    }
+}
--- src/Porter2023/Program.cs
+++ src/Porter2023/Program.cs
@@ -2,6 +2,20 @@
 using Porter2023.Libraries;
 
 int[] numeros = { 2147483647, 2147483647 };
-string expressao = "21 / 8 / 6";
+string expressao = "2 * 6";
 //Console.WriteLine(Calculadora.SomarArray(numeros));
-Console.WriteLine(Calculadora.CalcularExpressao(expressao));
+//Console.WriteLine(Calculadora.CalcularExpressao(expressao));
+
+IList<string> nomes = new List<string>();
+nomes.Add("Ana");
+nomes.Add("Ana");
+nomes.Add("Ana Carolina");
+nomes.Add("Ana");
+nomes.Add("Ana Paula");
+nomes.Add("Ana");
+nomes.Add("Ana Maria");
+
+nomes = ComparadorObjetos<string>.RemoverDuplicados(nomes);
+
+foreach (string nome in nomes)
+    Console.WriteLine(nome);
